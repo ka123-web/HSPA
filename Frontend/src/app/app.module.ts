d0d9030da8e066fb3ceacfe594dfbe6c50f4476d
ka123-web <cavi.vpm@gmail.com>
2021-02-22 16:08:56 -0500

    initial setup

---
+++ Frontend/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { PropertyListComponent } from './properties/property-list/property-list.component';
+import { PropertyCardComponent } from './properties/property-card/property-card-component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+
+
+
+@NgModule({
+  declarations: [	
+    AppComponent,
+    PropertyCardComponent,
+    PropertyListComponent,
+      NavBarComponent
+   ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
